163288b27ad8df81fa58d3d1e19ddc7b0d688119
TaigaSakaguchi <gucci.tennismath@gmail.com>
2020-04-15 10:15:03 +0900

    vue todolist

---
+++ index.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+  el: '#app',
+  data:{
+    newTask: '',
+    todoList: [],
+  },
+  methods:{
+    addNewTask(){
+      if(this.newTask == '') return;
+
+      this.todoList.push({
+        task: this.newTask,
+        isDone: false
+      });
+      this.newTask = '';
+
+    },
+    deleteTask(index){
+      this.todoList.splice(index,1);
+    }
+  },
+  computed: {
+    remains: function(){
+      var count = 0;
+      var todoLength = this.todoList.length;
+      for(var i = 0; i<todoLength; i++){
+        if(!this.todoList[i].isDone){
+          count++;
+        }
+      }
+      return count;
+    }
+  }
+})
